8a637b2b87232cc0e6a96f1decb0fea490ee4d76
Camila1953 <ayelenchavez1953@gmail.com>
2024-02-23 14:02:33 -0300

    ultimos ajustes

--- sript.js
+++ sript.js
@@ -1,6 +1,7 @@
 const menu = document.querySelector(".menu");
 const openMenu = document.querySelector(".open-menu");
 const closeMenu = document.querySelector(".close-menu");
+const navLinks = document.querySelectorAll(".menu-link")
 
 function toggleMenu() {
     menu.classList.toggle("menu_opened");
@@ -8,3 +9,9 @@ function toggleMenu() {
 
 openMenu.addEventListener("click", toggleMenu);
 closeMenu.addEventListener("click", toggleMenu);
+
+navLinks.forEach( link => {
+    link.addEventListener("click", function () {
+        menu.classList.remove("menu_opened");
+    });
+})
